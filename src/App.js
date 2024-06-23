26f12baaac6ce600cd2364bab4e7734839108673
Rudra Prasad <dipu13562@gmail.com>
2024-06-23 13:31:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Download from "./components/Download";
+
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <Hero/>
-      <Download/>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <Navbar />
+        <Hero />
+        <Download />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,50 +1,51 @@
-import React from 'react'
-import "./Hero.css"
+import React from "react";
+import "./Hero.css";
+
 function Hero() {
   return (
     <div className="container d-flex py-5">
-        <div className="box">
-             <img src="https://source.unsplash.com/200x200/?clothes,dresses" alt="Dresses"/>
-             <p>Dresses</p>
-        </div>
-       <div className="box">
-             <img src="https://source.unsplash.com/200x200/?shoes,footwares" alt="Shoes"/>
-             <p>Shoes</p>
-        </div>
-        <div className="box">
-             <img src="https://source.unsplash.com/200x200/?glasses,gogles" alt="Glasses"/>
-             <p>Glasses</p>
-        </div>
-        <div className="box">
-             <img src="https://source.unsplash.com/200x200/?watches" alt="Watches"/>
-             <p>Watches</p>
-        </div>
-        <div className="box">
-             <img src="https://source.unsplash.com/200x200/?jackets,sweaters" alt="Jackets"/>
-             <p>Jackets</p>
-        </div>
-        <div className="box">
-             <img src="https://source.unsplash.com/200x200/?laptops" alt="Laptops"/>
-             <p>Laptops</p>
-        </div> 
-        <div className="box">
-             <img src="https://source.unsplash.com/200x200/?headphones" alt="Headphones"/>
-             <p>Headphones</p>
-        </div>
-        <div className="box">
-             <img src="https://source.unsplash.com/200x200/?furnitures" alt="Furnitures"/>
-             <p>Furnitures</p>
-        </div> 
-        <div className="box">
-             <img src="https://source.unsplash.com/200x200/?gadgets" alt="Gadgets"/>
-             <p>Gadgets</p>
-        </div>
-        <div className="box">
-             <img src="https://source.unsplash.com/200x200/?makeup" alt="Beauty Products"/>
-             <p>Makeup Kits</p>
-        </div>
+      <div className="box">
+        <img src="https://fcity.in/images/products/124153289/obi5j_512.jpg" alt="Dresses" width="200" height="200"/>
+        <p> Dresses </p>
+      </div>
+      <div className="box">
+        <img src="https://5.imimg.com/data5/SELLER/Default/2022/11/YV/ZF/YO/116453489/white-casual-shoes-for-men.jpg" alt="Shoes" width="200" height="200"/>
+        <p> Shoes </p>
+      </div>
+      <div className="box">
+        <img src="https://img.freepik.com/free-photo/eyeglasses-sunglasses-reflecting-summer-elegance-design-generated-by-ai_188544-19634.jpg" alt="Glasses" width="200" height="200"/>
+        <p> Glasses </p>
+      </div>
+      <div className="box">
+        <img src="https://m.media-amazon.com/images/I/717ekHOBJ5L._AC_UY350_.jpg" alt="Watches" width="200" height="200"/>
+        <p> Watches </p>
+      </div>
+      <div className="box">
+        <img src="https://3.imimg.com/data3/AU/KI/MY-646899/men-s-jackets.jpg" alt="Jackets" width="200" height="200"/>
+        <p> Jackets </p>
+      </div>
+      <div className="box">
+        <img src="https://cdn.wallpapersafari.com/4/93/6pKVvS.jpg" alt="Laptops" width="200" height="200"/>
+        <p> Laptops </p>
+      </div>
+      <div className="box">
+        <img src="https://png.pngtree.com/background/20230527/original/pngtree-hd-headphone-on-black-background-with-pink-and-bright-blue-lights-picture-image_2760698.jpg" alt="Headphones" width="200" height="200"/>
+        <p> Headphones </p>
+      </div>
+      <div className="box">
+        <img src="https://content3.jdmagicbox.com/v2/comp/bangalore/s2/080pxx80.xx80.190323145046.m7s2/catalogue/national-furnituress-vijaya-bank-layout-bangalore-furniture-dealers-xrb12ka63n.jpg" alt="Furnitures" width="200" height="200"/>
+        <p> Furnitures </p>
+      </div>
+      <div className="box">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_i4Gx-xxskCRJ0cN95v9Bqakc2YWPzJIdLA&s" alt="Gadgets" width="200" height="200"/>
+        <p> Gadgets </p>
+      </div>
+      <div className="box">
+        <img src="https://vishubeautyparlour.com/blog/blogimages/makeup-product.jpg?v=1" alt="Beauty Products" width="200" height="200"/>
+        <p> Makeup Kits </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
